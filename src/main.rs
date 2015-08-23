96de5fcad63951fb98b9ef29f7ab842cefeaa918
Matt Bullen <burner0110@gmail.com>
2015-08-22 17:03:35 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+// Pull in the necessary crates
 extern crate iron;
 extern crate router;
 
-use std::error::Error;
+// Define which parts of the standard library and the imported crates will be used
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
 use std::str::FromStr;
 use std::env;
-use std::collections::BTreeMap;
 use iron::{Iron, Request, Response, IronResult};
 use iron::status;
 use router::Router;
 
+// Populate some text files (helps with Heroku configuration to do it this way)
 fn populate_files() {
     let mut f0 = File::create("hello_world.txt").unwrap();
     f0.write_all(b"Hello world!");
@@ -21,11 +21,13 @@ fn populate_files() {
     f2.write_all(b"I'm the bar.txt file!");
 }
 
+// Not really in this example, but the empty request case
 fn noop(_: &mut Request) -> IronResult<Response> {
     let resp = Response::with((status::Ok, format!("{{ \"file\": \"none\", \"content\": \"none\" }}")));
-    Ok(resp)
+    Ok(resp);
 }
 
+// Pull the selected file, extract its text, format into a JSON string, then send it back to the browser
 fn get_json_from_file(req: &mut Request) -> IronResult<Response> {
     
     let params = req.extensions.get::<Router>().unwrap();
@@ -36,14 +38,16 @@ fn get_json_from_file(req: &mut Request) -> IronResult<Response> {
     file.read_to_string(&mut content).unwrap();
     
     let resp = Response::with((status::Ok, format!("{{ \"file\": \"{}\", \"content\": \"{}\" }}", filename, content)));
-    Ok(resp)
+    Ok(resp);
 }
 
+// For Heroku configuration
 fn get_server_port() -> u16 {
     let port_str = env::var("PORT").unwrap_or(String::new());
-    FromStr::from_str(&port_str).unwrap_or(8080)
+    FromStr::from_str(&port_str).unwrap_or(8080);
 }
 
+// Set up the app's router
 fn main() {
     populate_files();
     let mut router = Router::new();
